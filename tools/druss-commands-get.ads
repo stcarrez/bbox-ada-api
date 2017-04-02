56114a2464772434091905a9c939b8d4da189972
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-02 10:42:33 +0200

    New package for the get command to execute a GET request and print the JSON raw result

---
+++ tools/druss-commands-get.ads
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------
+--  druss-commands-get -- Raw JSON API Get command
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Druss.Commands.Get is
+
+   type Command_Type is new Druss.Commands.Drivers.Command_Type with null record;
+
+   --  Execute the GET API operation and print the raw JSON result.
+   procedure Execute_Operation (Command   : in Command_Type;
+                                Operation : in String;
+                                Context   : in out Context_Type);
+
+   --  Execute a GET operation on the Bbox API and return the raw JSON result.
+   overriding
+   procedure Execute (Command   : in Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in Command_Type;
+                   Context   : in out Context_Type);
+
+end Druss.Commands.Get;
