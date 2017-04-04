de2f77c2bff0219b738c1260e30c1eb4476deab3
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-04 21:24:20 +0200

    Implement the Find_IP operation Implement the Save_Gateways operation

--- tools/druss-gateways.adb
+++ tools/druss-gateways.adb
@@ -17,10 +17,14 @@
 -----------------------------------------------------------------------
 with Util.Properties.Basic;
 with Util.Strings;
+with Util.Log.Loggers;
 with Bbox.API;
 package body Druss.Gateways is
 
    use Ada.Strings.Unbounded;
+
+   --  The logger
+   Log   : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Druss.Gateways");
 
    protected body Gateway_State is
       function Get_State return State_Type is
@@ -32,7 +36,13 @@ package body Druss.Gateways is
 
    function "=" (Left, Right : in Gateway_Ref) return Boolean is
    begin
-      return False;
+      if Left.Value = Right.Value then
+         return True;
+      elsif Left.Is_Null or Right.Is_Null then
+         return False;
+      else
+         return Left.Value.IP = Right.Value.IP;
+      end if;
    end "=";
 
    --  ------------------------------
@@ -44,20 +54,47 @@ package body Druss.Gateways is
    begin
       for I in 1 .. Count loop
          declare
-            Gw   : Gateway_Ref := Gateway_Refs.Create;
+            Gw   : constant Gateway_Ref := Gateway_Refs.Create;
             Base : constant String := "druss.bbox." & Util.Strings.Image (I);
          begin
             Gw.Value.Ip := Config.Get (Base & ".ip");
-            Gw.Value.Images := Config.Get (Base & ".images");
+            if Config.Exists (Base & ".images") then
+               Gw.Value.Images := Config.Get (Base & ".images");
+            end if;
             Gw.Value.Passwd := Config.Get (Base & ".password");
+            Gw.Value.Serial := Config.Get (Base & ".serial");
             List.Append (Gw);
+
+         exception
+            when Util.Properties.NO_PROPERTY =>
+               Log.Debug ("Ignoring gatway {0}", Base);
+         end;
+      end loop;
+   end Initialize;
+
+   --  ------------------------------
+   --  Save the list of gateways.
+   --  ------------------------------
+   procedure Save_Gateways (Config : in out Util.Properties.Manager;
+                            List   : in Druss.Gateways.Gateway_Vector) is
+      Pos : Natural := 0;
+   begin
+      for Gw of List loop
+         Pos := Pos + 1;
+         declare
+            Base : constant String := "druss.bbox." & Util.Strings.Image (Pos);
+         begin
+            Config.Set (Base & ".ip", Gw.Value.Ip);
+            Config.Set (Base & ".password", Gw.Value.Passwd);
+            Config.Set (Base & ".serial", Gw.Value.Serial);
 
          exception
             when Util.Properties.NO_PROPERTY =>
                null;
          end;
       end loop;
-   end Initialize;
+      Util.Properties.Basic.Integer_Property.Set (Config, "druss.bbox.count", Pos);
+   end Save_Gateways;
 
    --  ------------------------------
    --  Refresh the information by using the Bbox API.
@@ -69,11 +106,16 @@ package body Druss.Gateways is
          return;
       end if;
       Box.Set_Server (To_String (Gateway.IP));
-      Box.Login (To_String (Gateway.Passwd));
+      if Ada.Strings.Unbounded.Length (Gateway.Passwd) > 0 then
+         Box.Login (To_String (Gateway.Passwd));
+      end if;
       Box.Get ("wan/ip", Gateway.Wan);
       Box.Get ("lan/ip", Gateway.Lan);
       Box.Get ("device", Gateway.Device);
       Box.Get ("wireless", Gateway.Wifi);
+      if Gateway.Device.Exists ("device.serialnumber") then
+         Gateway.Serial := Gateway.Device.Get ("device.serialnumber");
+      end if;
    end Refresh;
 
    --  ------------------------------
@@ -94,5 +136,22 @@ package body Druss.Gateways is
          Process (G.Value.all);
       end loop;
    end Iterate;
+
+   Null_Gateway : Gateway_Ref;
+
+   --  ------------------------------
+   --  Find the gateway with the given IP address.
+   --  ------------------------------
+   function Find_IP (List : in Gateway_Vector;
+                     IP   : in String) return Gateway_Ref is
+   begin
+      for G of List loop
+         if G.Value.IP = IP then
+            return G;
+         end if;
+      end loop;
+      --  raise Not_Found;
+      return Null_Gateway;
+   end Find_IP;
 
 end Druss.Gateways;
