df086b538d0d341143f1c187e4a5bc8815c53e83
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2017-04-07 22:24:26 +0200

    New package for the device command to list the devices known by the Bbox

---
+++ tools/druss-commands-devices.ads
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------
+--  druss-commands-devices -- Print information about the devices
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Druss.Commands.Devices is
+
+   type Command_Type is new Druss.Commands.Drivers.Command_Type with null record;
+
+   procedure Do_List (Command   : in Command_Type;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Execute a status command to report information about the Bbox.
+   overriding
+   procedure Execute (Command   : in Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in Command_Type;
+                   Context   : in out Context_Type);
+
+end Druss.Commands.Devices;
