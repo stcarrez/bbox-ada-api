77119f2b27d96aa3979a4f4dadf9e6ff1b39329b
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2017-04-07 22:27:06 +0200

    Define the ping command to ask the Bbox to ping the known devices

---
+++ tools/druss-commands-ping.ads
@@ -0,0 +1,39 @@
+-----------------------------------------------------------------------
+--  druss-commands-ping -- Ping devices from the gateway
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package Druss.Commands.Ping is
+
+   type Command_Type is new Druss.Commands.Drivers.Command_Type with null record;
+
+   procedure Do_Ping (Command   : in Command_Type;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Execute a ping from the gateway to each device.
+   overriding
+   procedure Execute (Command   : in Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Write the help associated with the command.
+   overriding
+   procedure Help (Command   : in Command_Type;
+                   Context   : in out Context_Type);
+
+end Druss.Commands.Ping;
