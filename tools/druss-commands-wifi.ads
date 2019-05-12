60f5c8fd887d0ea8494adb87aedbc24326ceffba
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-05-12 17:43:57 +0200

    Change Help command to accept in out command

--- tools/druss-commands-wifi.ads
+++ tools/druss-commands-wifi.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  druss-commands-wifi -- Wifi related commands
---  Copyright (C) 2017, 2018 Stephane Carrez
+--  Copyright (C) 2017, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,7 +39,7 @@ package Druss.Commands.Wifi is
 
    --  Write the help associated with the command.
    overriding
-   procedure Help (Command   : in Command_Type;
+   procedure Help (Command   : in out Command_Type;
                    Context   : in out Context_Type);
 
 end Druss.Commands.Wifi;
