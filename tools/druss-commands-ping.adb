35cb08c934bd29bbb252fb65d327f007344df304
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-05-12 17:43:37 +0200

    Change Help command to accept in out command

--- tools/druss-commands-ping.adb
+++ tools/druss-commands-ping.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  druss-commands-devices -- Print information about the devices
---  Copyright (C) 2017, 2018 Stephane Carrez
+--  Copyright (C) 2017, 2018, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -137,7 +137,7 @@ package body Druss.Commands.Ping is
    begin
       if Args.Get_Count > 1 then
          Context.Console.Notice (N_USAGE, "Too many arguments to the command");
-         Druss.Commands.Driver.Usage (Args);
+         Druss.Commands.Driver.Usage (Args, Context);
       elsif Args.Get_Count = 0 then
          Command.Do_Ping (Args, DEVICE_ALL, Context);
       elsif Args.Get_Argument (1) = "all" then
@@ -148,7 +148,7 @@ package body Druss.Commands.Ping is
          Command.Do_Ping (Args, DEVICE_INACTIVE, Context);
       else
          Context.Console.Notice (N_USAGE, "Invalid argument: " & Args.Get_Argument (1));
-         Druss.Commands.Driver.Usage (Args);
+         Druss.Commands.Driver.Usage (Args, Context);
       end if;
    end Execute;
 
@@ -156,7 +156,7 @@ package body Druss.Commands.Ping is
    --  Write the help associated with the command.
    --  ------------------------------
    overriding
-   procedure Help (Command   : in Command_Type;
+   procedure Help (Command   : in out Command_Type;
                    Context   : in out Context_Type) is
       pragma Unreferenced (Command);
       Console : constant Druss.Commands.Consoles.Console_Access := Context.Console;
